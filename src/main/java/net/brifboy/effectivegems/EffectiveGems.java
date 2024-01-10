9c124e219604bed806132e5a56fdb714e6be72f5
Brian <brian.a.farastad@gmail.com>
2024-01-10 15:33:56 +0100

    First Git

---
+++ src/main/java/net/brifboy/effectivegems/EffectiveGems.java
@@ -0,0 +1,17 @@
+package net.brifboy.effectivegems;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EffectiveGems implements ModInitializer {
+
+	public static final String MOD_ID = "effectivegems";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/brifboy/effectivegems/EffectiveGemsClient.java
@@ -0,0 +1,10 @@
+package net.brifboy.effectivegems;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EffectiveGemsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
